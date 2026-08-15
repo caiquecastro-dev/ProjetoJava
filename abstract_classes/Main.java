6d5d0584690feb27b50aaa644382e36b6208e1a3
caiquecastro-dev <caiquecastro313@gmail.com>
2026-08-15 22:24:29 +0000

    a implementação dos métodos abstratos aplica o conceito de polimorfismo. Quando um método é implementado na classe abstrata, ele é chamado de método concreto. De forma semelhante, quando uma subclasse herda as características de uma classe abstrata e implementa os métodos dela, é chamada de classe concreta.

---
+++ abstract_classes/Main.java
@@ -0,0 +1,54 @@
+abstract class Animal {
+    // metodo abstrato
+    public abstract void emitirSom();
+
+    // metodo concreto
+    public void dormir() {
+        System.out.println("Zzzz...");
+    }
+
+    public void tipoDeAnimal() {
+        System.out.println("É um animal selvagem");
+    }
+}
+
+// subclasse concreta
+class Cachorro extends Animal {
+    @Override
+    public void emitirSom() {
+        System.out.println("Latir!");
+    }
+}
+
+// subclasse concreta
+class Gato extends Animal {
+    @Override
+    public void emitirSom() {
+        System.out.println("Miar!");
+    }
+}
+
+// subclasse concreta
+class Leao extends Animal {
+    @Override
+    public void emitirSom() {
+        System.out.println("Rugir");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Animal cachorro = new Cachorro();
+        Animal gato = new Gato();
+        Animal leao = new Leao();
+
+        cachorro.emitirSom();
+        cachorro.dormir();
+
+        gato.emitirSom();
+        gato.dormir();
+
+        leao.emitirSom();
+        leao.dormir();
+        leao.tipoDeAnimal();
+    }
+}
